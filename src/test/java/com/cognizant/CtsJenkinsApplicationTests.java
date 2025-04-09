aa15bac62dd292a663fb4cce2d3782fdbebe6d80
b5user01 <b5user01@rpsconsulting.in>
2025-04-09 08:31:39 +0530

    first commit

---
+++ src/test/java/com/cognizant/CtsJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cognizant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtsJenkinsApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(CtsJenkinsApplication.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case started executing...");
+		assertEquals(true, true);
+	}
+
+}
